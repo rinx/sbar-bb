d63dd223095a864fc922e7587d9351804b008466
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-04-08 23:32:51 +0900

    docs: update example script
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- examples/example.clj
+++ examples/example.clj
@@ -24,9 +24,9 @@
               :label.padding_right 4}))
 
 (defn setup-time []
-  (sketchybar/exec
-   (sketchybar/add-item :time :right)
-   (sketchybar/set
+  (s/exec
+   (s/add-item :time :right)
+   (s/set
     :time
     {:icon.padding_right 0
      :label.padding_left 0})))
@@ -38,12 +38,13 @@
 
 (defn update-time []
   (let [time (tick/format (tick/formatter "HH:mm") (tick/time))]
-    (sketchybar/exec
-     (sketchybar/set :time {:label time}))))
+    (s/exec
+     (s/set :time {:label time}))))
 
 (defn -main [& args]
   (init)
 
+  ;; update `time` widget every 10 seconds
   (event-loop
    {:time {:fn update-time
            :duration-ms 10000}}))
